83e9b3fae791eded40826273c7d8d100c76fed22
Jacob Mutale <mutale1808@gmail.com>
2022-04-01 12:42:30 +0200

    finished testing user model class and implementation

--- scopetik/lib/features/login/models/user_model.dart
+++ scopetik/lib/features/login/models/user_model.dart
@@ -7,18 +7,34 @@ class UserModel {
   final String refreshToken;
 
   //The number of seconds in which the ID token expires.
-  final String expiresln;
+  final String expiresIn;
 
   //The uid of the authenticated user.
   final String localId;
   final bool registered;
 
+  final String displayName;
+
   UserModel({
     required this.idToken,
     required this.email,
-    required this.expiresln,
+    required this.expiresIn,
     required this.localId,
     required this.refreshToken,
     required this.registered,
+    required this.displayName,
   });
+
+  //from Json function
+ factory  UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      idToken: json['idToken'],
+      email: json['email'],
+      expiresIn: json['expiresIn'],
+      localId: json['localId'],
+      refreshToken: json['refreshToken'],
+      registered: json['registered'],
+      displayName: json['displayName'],
+    );
+  }
 }
